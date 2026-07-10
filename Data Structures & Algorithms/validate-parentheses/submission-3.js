b183a94ec0d939ff6af08a20efb2a0d5747427c4
Manan <137396130+thisis-manan@users.noreply.github.com>
2026-07-10 23:56:45 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let a= ""
+        for(let i=0; i<s.length; i++){
+            if ( s[i]==="{" ||  s[i]==="[" || s[i] === "("){
+            a+=s[i]
+            continue;}
+            else if( a[a.length-1]==="{" && s[i]==="}" ||a[a.length-1]==="[" && s[i]==="]" ||a[a.length-1]==="(" && s[i]===")" ){
+            a = a.slice(0, -1);
+
+            continue;
+            }
+            else{
+                return false
+            }
+        }
+        if (a.length ===0){
+            return true}
+        else{
+            return false
+        }
+    }
+}
