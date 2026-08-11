a237374b631ca346f7a8c90b8ba526cca43fc14e
Manan <thisis.mananagarwal@gmail.com>
2026-08-11 08:53:26 +0530

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let l=0
+        let r=numbers.length-1
+
+        while(l<r){
+            let temp =numbers[l]+numbers[r]
+            if(temp==target){
+                return[l+1,r+1]
+            }
+            else if(temp<target){
+                l++
+            }
+            else {
+                r--
+            }
+        }
+    }
+}
